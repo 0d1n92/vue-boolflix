f72966b2d4e3e90a690a31eab68392c9723936cb
enrico <rombaldonienrico@gmail.com>
2020-12-18 19:03:27 +0100

    search

--- js/script.js
+++ js/script.js
@@ -1,33 +1,42 @@
+const api="https://api.themoviedb.org/3/search/movie"
+
 var  app= new Vue({
   el: '#app',
 
+
   data: {
-    title:'back',
+    title:'',
     films:[],
     img_url: "https://image.tmdb.org/t/p/w220_and_h330_face/",
 
   },
-  mounted:function () {
-  //   axios.get('/user', {
-  //   params: {
-  //     ID: 12345
-  //   }
-  // })
-  // .then(function (response) {
-  //   console.log(response);
-  // })
-    axios.get('https://api.themoviedb.org/3/search/movie', {
-        params: {
-          api_key: "498c9ec3394d0a225e5b99e29b024805",
-          query:this.title,
+  methods: {
+    search:function (array) {
+      let self= this;
+    if (self.title!="") {
+      axios.get('https://api.themoviedb.org/3/search/movie', {
+          params: {
+            api_key: "498c9ec3394d0a225e5b99e29b024805",
+            query:self.title,
+          }
         }
-      }
-    ).then(
-      (information) => {
-        let self= this;
-        this.films= information.data.results;
-        console.log( this.films);
+      ).then(
+        (information) => {
+         this.films= information.data.results;
+      });
+    }
 
-    });
-  }
+    }
+  },
+  // mounted:function () {
+  // //   axios.get('/user', {
+  // //   params: {
+  // //     ID: 12345
+  // //   }
+  // // })
+  // // .then(function (response) {
+  // //   console.log(response);
+  // // })
+  //
+  // }
 })
