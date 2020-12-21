cb07db21d8599e9d7f3a9369ee7de10963e71ca4
enrico <rombaldonienrico@gmail.com>
2020-12-21 23:40:57 +0100

    promises All

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-//Milestone 2:
+// Milestone 3:
+// In questa milestone come prima cosa sostituiamo la copertina placeholder con quella vera del film o della serie al nostro elenco. Ci viene passata dall’API solo la parte finale dell’URL, questo perché poi potremo generare da quella porzione di URL tante dimensioni diverse. Dovremo prendere quindi l’URL base delle immagini di TMDB: https://image.tmdb.org/t/p/ per poi aggiungere la dimensione che vogliamo generare (troviamo tutte le dimensioni possibili a questo link: https://www.themoviedb.org/talk/53c11d4ec3a3684cf4006400) per poi aggiungere la parte finale dell’URL passata dall’API.
+// Utilizziamo un’immagine placeholder quando non riceviamo una copertina dall’API.
+// Esempio di URL:
+// https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png
+//
 
-// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
 // Qui un esempio di chiamata per le serie tv:
 // https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs
 var  app= new Vue({
@@ -18,32 +22,60 @@ var  app= new Vue({
   methods: {
     search:function (array) {
       let self= this;
-      let apiFilms= "https://api.themoviedb.org/3/search/movie?api_key=498c9ec3394d0a225e5b99e29b024805&query=trono%20di%20&language=it-IT";
-      let apiSeries=  "https://api.themoviedb.org/3/search/tv?api_key=498c9ec3394d0a225e5b99e29b024805&query=trono%20di%20spa&language=it-IT";
+      const apiKey="498c9ec3394d0a225e5b99e29b024805";
+      // let apiFilms= axios.get('https://api.themoviedb.org/3/search/tv', {
+      //   params: {
+      //     api_key:apiKey,
+      //     query: self.title,
+      //     language:"it-IT",
+      //   }
+      // });
+      //
+      // let apiSeries= axios.get('https://api.themoviedb.org/3/search/tv', {
+      //   params: {
+      //     api_key:apiKey,
+      //     query: self.title,
+      //     language:"it-IT",
+      //   }
+      // });
 
       // Promise.all([axios.get(apiFilms),axios.get(apiSeries)]).then(function(values) {
       //   console.log(values);
       // });
 
-       if (self.title!="" && self.title.length >= 3) {
-      Promise.all([axios.get(apiSeries),axios.get(apiFilms)]).then(
+      if (self.title!="" && self.title.length >= 3) {
+        // Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
+        Promise.all([
+          axios.get('https://api.themoviedb.org/3/search/movie', {
+            params: {
+              api_key:apiKey,
+              query: self.title,
+              language:"it-IT",
+            }
+          }),
+          axios.get('https://api.themoviedb.org/3/search/tv', {
+            params: {
+              api_key:apiKey,
+              query: self.title,
+              language:"it-IT",
+            }
+          })
+        ])
+        .then(
           function (information) {
             const informationMarge=[...information[0].data.results,...information[1].data.results,];
-           setTimeout(function (){
+            setTimeout(function (){
             self.films= self.leanguagesFlag(informationMarge);
-                console.log(self.films[1].flag_img);
             self.films.forEach((item, i) => {
               if (self.films.length > 0 ) {
               item.vote_average=self.voteAverageRound(informationMarge[i].vote_average);
               }
             });
-
            },2000);
-
         });
-
       }
      },
+     // Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (troviamo le icone in FontAwesome).
     voteAverageRound: function (vote) {
       // Math.Round(value * 2, MidpointRounding.AwayFromZero) / 2
       // voteRound=(Math.round(vote *2,  0.5) / 2)/2; for 0.5
