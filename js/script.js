a22c0516cb08be6d196b549eaa5a0f5381040a50
enrico <rombaldonienrico@gmail.com>
2020-12-21 22:55:55 +0100

    serietv

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 //Milestone 2:
 
-// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
 // Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
 // Qui un esempio di chiamata per le serie tv:
 // https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs
@@ -19,32 +18,32 @@ var  app= new Vue({
   methods: {
     search:function (array) {
       let self= this;
-      if (self.title!="" && self.title.length >= 3) {
-        axios.get('https://api.themoviedb.org/3/search/movie', {
-            params: {
-              api_key: "498c9ec3394d0a225e5b99e29b024805",
-              query:self.title,
-              language: "it-IT",
+      let apiFilms= "https://api.themoviedb.org/3/search/movie?api_key=498c9ec3394d0a225e5b99e29b024805&query=trono%20di%20&language=it-IT";
+      let apiSeries=  "https://api.themoviedb.org/3/search/tv?api_key=498c9ec3394d0a225e5b99e29b024805&query=trono%20di%20spa&language=it-IT";
 
-            }
-          }
-        ).then(
-          (information) => {
-            let movieData=information.data.results;
+      // Promise.all([axios.get(apiFilms),axios.get(apiSeries)]).then(function(values) {
+      //   console.log(values);
+      // });
+
+       if (self.title!="" && self.title.length >= 3) {
+      Promise.all([axios.get(apiSeries),axios.get(apiFilms)]).then(
+          function (information) {
+            const informationMarge=[...information[0].data.results,...information[1].data.results,];
            setTimeout(function (){
-            self.films= self.leanguagesFlag(movieData);
-
+            self.films= self.leanguagesFlag(informationMarge);
                 console.log(self.films[1].flag_img);
             self.films.forEach((item, i) => {
               if (self.films.length > 0 ) {
-              item.vote_average=self.voteAverageRound(movieData[i].vote_average);
+              item.vote_average=self.voteAverageRound(informationMarge[i].vote_average);
               }
             });
 
            },2000);
+
         });
+
       }
-    },
+     },
     voteAverageRound: function (vote) {
       // Math.Round(value * 2, MidpointRounding.AwayFromZero) / 2
       // voteRound=(Math.round(vote *2,  0.5) / 2)/2; for 0.5
@@ -54,7 +53,6 @@ var  app= new Vue({
     leanguagesFlag: function (array) {
 
       array.forEach((item, i) => {
-
         if(item.original_language ==="en") {
           item.flag_img="uk-16.png";
         } else if (item.original_language ==="it") {
@@ -66,5 +64,6 @@ var  app= new Vue({
       });
         return array;
     }
+
   }
 });
